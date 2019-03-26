368f7aad5c7f007e84a9fcbd19bb209520590d01
Tønnes <tonnes.roren@gmail.com>
2019-03-26 21:51:07 +0100

    Using curl instead of nping/nmap

--- pingScriptFixed.sh
+++ pingScriptFixed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -·
+declare host="aqueous-garden-80071.herokuapp.com"
 while [ 1 ]
 do
     currenttime=$(date +%H:%M)
 
     if [[ "$currenttime" > "06:00" ]] || [[ "$currenttime" < "23:59"  ]]; then
-        echo "|| $currenttime - Pining: $host"
-        nping -c 1 --tcp -p 80,433 aqueous-garden-80071.herokuapp.com ; echo $?
+        curl -A "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" --silent --head aqueous-garden-80071.herokuapp.com ; echo $?
     else
         echo "xx $currenttime - Outside of operation time."
     fi
